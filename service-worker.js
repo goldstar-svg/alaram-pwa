9c39fccc03f76f4e0b10940710fe436ad3a39223
goldstar-svg <kgsjus@gmail.com>
2025-09-16 20:32:12 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,46 @@
+const CACHE_NAME = "alarm-pwa-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./sound1.mp3",
+  "./sound2.mp3",
+  "./sound3.mp3",
+  "./sound4.mp3",
+  "./sound5.mp3",
+  "./sound6.mp3",
+  "./sound7.mp3"
+];
 
+// 설치 이벤트: 캐시 저장
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 요청 이벤트: 캐시 우선
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 활성화 이벤트: 오래된 캐시 삭제
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
