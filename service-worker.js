7eff1b7db61754e340232899673a3ab12ffd8d68
goldstar-svg <kgsjus@gmail.com>
2025-09-19 17:01:34 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,22 @@
-const CACHE_NAME = "alarm-pwa-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./sound1.mp3",
-  "./sound2.mp3",
-  "./sound3.mp3",
-  "./sound4.mp3",
-  "./sound5.mp3",
-  "./sound6.mp3",
-  "./sound7.mp3"
-];
-
-// 설치 이벤트: 캐시 저장
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('alarm-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './radar_like.mp3',
+        './beacon_like.mp3',
+        './chime_like.mp3',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
     })
   );
 });
 
-// 요청 이벤트: 캐시 우선
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// 활성화 이벤트: 오래된 캐시 삭제
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      )
-    )
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
